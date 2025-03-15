51281a00dd3aaa6d33090ee47f6e7bdede0c9341
ShimuGuyue <2147539031@qq.com>
2025-03-15 17:02:01 +0800

    爱莉希雅区域大小和页脚微调

--- obj/Debug/net9.0/Home-Shimu_Guyue.AssemblyInfo.cs
+++ obj/Debug/net9.0/Home-Shimu_Guyue.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Home-Shimu_Guyue")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edaca8a9f4d0e202d57065e5685c17cde888d15a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d99f6d897277a245552633dc744ca70c9244ee94")]
 [assembly: System.Reflection.AssemblyProductAttribute("Home-Shimu_Guyue")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Home-Shimu_Guyue")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
