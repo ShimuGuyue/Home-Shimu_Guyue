2e40d058616997266ef92a080f9736b21951201f
ShimuGuyue <2147539031@qq.com>
2025-03-15 15:26:44 +0800

    导航栏微调

--- obj/Debug/net9.0/Home-Shimu_Guyue.AssemblyInfo.cs
+++ obj/Debug/net9.0/Home-Shimu_Guyue.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Home-Shimu_Guyue")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11754ee0680372c7fd3ee6b216fbb53b4596204a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df89b3fc5e725684ba06a61db42f2a7709e2b337")]
 [assembly: System.Reflection.AssemblyProductAttribute("Home-Shimu_Guyue")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Home-Shimu_Guyue")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
